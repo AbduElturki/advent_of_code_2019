46d537c88590eac237674fd154d6b71d1403371e
Abdu Elturki <abdussalam@elturki.com>
2019-12-22 23:20:17 +0000

    Nicer display

--- 13/main.py
+++ 13/main.py
@@ -1,15 +1,16 @@
 import sys
+import time
 sys.path.append('../intcodeMachine')
 
 from intcodeMachine import intcodeMachine
 
 def coloredTile(tile):
     if tile == 1:
-        return '\033[91m1\033[0m'
+        return '\033[91m█\033[0m'
     elif tile == 2:
-        return '\033[93m2\033[0m'
+        return '\033[93m■\033[0m'
     elif tile == 3:
-        return '\033[94m-\033[0m'
+        return '\033[94m▀\033[0m'
     elif tile == 4:
         return '\033[92mO\033[0m'
     elif tile == 0:
@@ -36,11 +37,11 @@ def countBlockTiles():
 def runArcadeAi():
     highscore = 0
     tiles = [[0 for _ in range(37)] for _ in range(50)]
+    toPrint = [ None for _ in range(50) ]
     def aiPong():
         for y in range(len(tiles)):
             for x in range(len(tiles[y])):
                 if tiles[y][x] == 4:
-                    print(x,y)
                     ballLoc = x
                 if tiles[y][x] == 3:
                     padLoc = x
@@ -57,18 +58,19 @@ def runArcadeAi():
         y = system.run()
         if x == -1 and y == 0:
             highscore = system.run()
-            print(highscore)
-            ai = True
         else:
             tile = system.run()
         if x == None or y == None or tile == None:
            break
         tiles[y][x] = tile
+        print(chr(27) + "[2J")
         for i in range(50):
-            print(*list(map(coloredTile, tiles[i])))
-        print('-' * 37)
-        print('Score: ', highscore)
-        print('-' * 37)
+            toPrint[i] = "".join(list(map(coloredTile, tiles[i])))
+        sys.stdout.write('\n'.join(toPrint) + '\n' + '-' * 37 + '\n' +
+                         'Score: ' + str(highscore) + '\n' +
+                         '-' * 37 + '\n')
+        time.sleep(.001)
+        sys.stdout.flush()
     return highscore
 
 def runArcade():
@@ -80,15 +82,16 @@ def runArcade():
         y = system.run()
         if x == -1 and y == 0:
             highscore = system.run()
-            print(highscore)
         else:
             tile = system.run()
         if x == None or y == None or tile == None:
            break
         tiles[y][x] = tile
         for i in range(50):
-            print(*list(map(coloredTile, tiles[i])))
+            print(*list(map(coloredTile, tiles[i])),sep='')
         print('-------------------------------------')
+        print('Score: ', highscore)
+        print('-' * 37)
 
 if __name__ == '__main__':
     highscore = runArcadeAi()
